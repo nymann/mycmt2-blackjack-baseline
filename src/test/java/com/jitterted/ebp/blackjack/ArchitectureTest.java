445edf52d0f0177f32f7b194b7b4e27c23b7fb16
Kristian Jakobsen <kristian@nymann.dev>
2026-02-24 23:49:50 +0100

    Convert ArchUnit tests from @ArchTest fields to @Test methods
    archunit-junit5 engine doesn't respect JUnit 6 @Tag filtering,
    so architecture tests ran even when excluded. Using regular @Test
    methods puts Jupiter in control of discovery and tag filtering.
    Also add !architecture exclusion to I/O-Free Tests run config.

--- src/test/java/com/jitterted/ebp/blackjack/ArchitectureTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/ArchitectureTest.java
@@ -3,53 +3,72 @@ package com.jitterted.ebp.blackjack;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.core.importer.ImportOption;
-import com.tngtech.archunit.junit.AnalyzeClasses;
-import com.tngtech.archunit.junit.ArchTest;
-import com.tngtech.archunit.lang.ArchRule;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
-@SuppressWarnings("unused")
 @Tag("architecture")
-@AnalyzeClasses(packages = "com.jitterted.ebp.blackjack", importOptions = ImportOption.DoNotIncludeTests.class)
 class ArchitectureTest {
 
-    @ArchTest
-    static final ArchRule no_direct_random_usage =
-            noClasses().should().dependOnClassesThat().haveFullyQualifiedName("java.util.Random");
+    private static final JavaClasses classes = new ClassFileImporter()
+            .withImportOption(new ImportOption.DoNotIncludeTests())
+            .importPackages("com.jitterted.ebp.blackjack");
 
-    @ArchTest
-    static final ArchRule no_system_exit_calls = noClasses()
-            .should()
-            .callMethod(System.class, "exit", int.class)
-            .because("System.exit() should never be called in production code");
+    @Test
+    void no_direct_random_usage() {
+        noClasses()
+                .should()
+                .dependOnClassesThat()
+                .haveFullyQualifiedName("java.util.Random")
+                .check(classes);
+    }
 
-    @ArchTest
-    static final ArchRule all_classes_must_reside_in_hexagonal_packages = classes()
-            .that()
-            .doNotHaveFullyQualifiedName("com.jitterted.ebp.blackjack.Game")
-            .should()
-            .resideInAnyPackage("..domain..", "..application..", "..adapter..")
-            .because(
-                    "all production classes (except the entrypoint) must be organized into domain, application, or adapter packages");
+    @Test
+    void no_system_exit_calls() {
+        noClasses()
+                .should()
+                .callMethod(System.class, "exit", int.class)
+                .because("System.exit() should never be called in production code")
+                .check(classes);
+    }
 
-    @ArchTest
-    static final ArchRule domain_must_not_depend_on_adapters_or_application = noClasses()
-            .that()
-            .resideInAPackage("..domain..")
-            .should()
-            .dependOnClassesThat()
-            .resideInAnyPackage("..adapter..", "..application..")
-            .allowEmptyShould(true)
-            .because("domain must not depend on adapters or application");
+    @Test
+    void all_classes_must_reside_in_hexagonal_packages() {
+        classes()
+                .that()
+                .doNotHaveFullyQualifiedName("com.jitterted.ebp.blackjack.Game")
+                .should()
+                .resideInAnyPackage("..domain..", "..application..", "..adapter..")
+                .because(
+                        "all production classes (except the entrypoint) must be organized into domain, application, or adapter packages")
+                .check(classes);
+    }
 
-    @ArchTest
-    static final ArchRule application_must_not_depend_on_adapters = noClasses()
-            .that()
-            .resideInAPackage("..application..")
-            .should()
-            .dependOnClassesThat()
-            .resideInAPackage("..adapter..")
-            .allowEmptyShould(true)
-            .because("application must not depend on adapters");
+    @Test
+    void domain_must_not_depend_on_adapters_or_application() {
+        noClasses()
+                .that()
+                .resideInAPackage("..domain..")
+                .should()
+                .dependOnClassesThat()
+                .resideInAnyPackage("..adapter..", "..application..")
+                .allowEmptyShould(true)
+                .because("domain must not depend on adapters or application")
+                .check(classes);
+    }
+
+    @Test
+    void application_must_not_depend_on_adapters() {
+        noClasses()
+                .that()
+                .resideInAPackage("..application..")
+                .should()
+                .dependOnClassesThat()
+                .resideInAPackage("..adapter..")
+                .allowEmptyShould(true)
+                .because("application must not depend on adapters")
+                .check(classes);
+    }
 }
